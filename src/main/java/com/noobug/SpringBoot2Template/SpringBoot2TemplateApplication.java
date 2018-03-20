82e3b051bb1b600620408b76d71f03100ae2ead7
小王子 <976228284@qq.com>
2018-03-20 09:22:38 +0800

    首次提交

---
+++ src/main/java/com/noobug/SpringBoot2Template/SpringBoot2TemplateApplication.java
@@ -0,0 +1,12 @@
+package com.noobug.SpringBoot2Template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2TemplateApplication.class, args);
+	}
+}
