141abf9167b6f14ea0bb5a27f50607fa38bc03d0
小王子(基鸿) <976228284@qq.com>
2018-03-20 18:45:41 +0800

    修改配置文件

--- src/main/java/com/noobug/SpringBoot2Template/web/rest/AdminResource.java
+++ src/main/java/com/noobug/SpringBoot2Template/web/rest/AdminResource.java
@@ -1,10 +1,16 @@
 package com.noobug.SpringBoot2Template.web.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminResource {
 
+    @GetMapping("/get1")
+    public Mono<String> get1(){
+        return null;
+    }
 }
